9126c972c2bfeb786323af28ae7daa3b243a451e
Oleksandr Riznyk <OleksandrRiznyk01@gmail.com>
2024-02-28 20:34:46 +0200

    Add DBF file processing.

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/tadvi/dbf"
 	"os"
+	"path/filepath"
 	"strings"
 	"sync"
 	"time"
@@ -10,33 +12,83 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	var columnValue, column string
 
-	for len(os.Args) < 2 {
+	checkForDroppedFiles(os.Args)
+	checkForOtherFormats(os.Args)
+
+	fmt.Printf("please enter a column name to change:\n")
+	_, err := fmt.Scan(&column)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("please enter a value and press enter:\n")
+	_, err = fmt.Scan(&columnValue)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("you entered column:%s, value:%s\n", column, columnValue)
+
+	wg.Add(len(os.Args) - 1)
+	go func() {
+		for _, filePath := range os.Args[1:] {
+			go processDBFFile(filePath, column, columnValue, &wg)
+		}
+	}()
+
+	wg.Wait()
+	<-time.After(15 * time.Second)
+}
+
+func processDBFFile(filePath string, col string, val string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("processing file: %s\n", filePath)
+	_, fileName := filepath.Split(filePath)
+	dirPath := filepath.Dir(filePath)
+	changedFilesDir := filepath.Join(dirPath, "changed")
+	pathForTheChangedFiles := filepath.Join(dirPath, "changed", fileName)
+
+	t, err := dbf.LoadFile(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i := 0; i < t.NumRecords(); i++ {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("panic occurred:", r)
+			}
+		}()
+		t.SetFieldValueByName(i, col, val)
+	}
+
+	_, err = os.Stat(changedFilesDir)
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(changedFilesDir, os.ModePerm)
+		if err != nil {
+			fmt.Println("Error creating folders:", err)
+			return
+		}
+	}
+
+	err = t.SaveFile(pathForTheChangedFiles)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+func checkForDroppedFiles(files []string) {
+	for len(files) < 2 {
 		fmt.Println("please drop files on the EXE file")
 		<-time.After(2 * time.Second)
 		os.Exit(0)
 	}
-
-	for _, filePath := range os.Args[1:] {
+}
+func checkForOtherFormats(files []string) {
+	for _, filePath := range files[1:] {
 		if !strings.HasSuffix(filePath, "DBF") {
 			fmt.Println("please drop only DBF files, try again")
 			<-time.After(2 * time.Second)
 			os.Exit(0)
 		}
 	}
-
-	wg.Add(len(os.Args) - 1)
-	go func() {
-		for _, filePath := range os.Args[1:] {
-			go processDBFFile(filePath, &wg)
-		}
-	}()
-
-	wg.Wait()
-	<-time.After(3 * time.Second)
-}
-
-func processDBFFile(filePath string, wg *sync.WaitGroup) {
-	fmt.Printf("Processing file: %s\n", filePath)
-	wg.Done()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module dbf-column-filler
 
 go 1.21
+
+require github.com/tadvi/dbf v0.0.0-20160415171023-7f098c0fd31f
