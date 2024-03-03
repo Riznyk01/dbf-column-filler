8b89bcf5b23a6a1b23e8573b25e0a41375063ce9
Oleksandr Riznyk <OleksandrRiznyk01@gmail.com>
2024-03-03 23:49:15 +0200

    Refactored code by consolidating text into constants.

--- cmd/main.go
+++ cmd/main.go
@@ -12,8 +12,21 @@ import (
 )
 
 const (
-	startMessage = "Hello 👋 \nThis program is designed to modify column values in either a single file or a group of files that you drag and drop onto the executable file. \nSimply drag and drop the files onto the executable, enter the field name, and specify the new value. \nThe modified files will be saved in the 'changed' directory within the same folder where the original files are located.\n"
-	logFileName  = "error_log.txt"
+	startMessage         = "Hello 👋 \nThis program is designed to modify column values in either a single file or a group of files that you drag and drop onto the executable file. \nSimply drag and drop the files onto the executable, enter the field name, and specify the new value. \nThe modified files will be saved in the 'changed' directory within the same folder where the original files are located.\n"
+	outputFolder         = "changed"
+	logFileName          = "error_log.txt"
+	fileExt              = "DBF"
+	failedToOpen         = "Failed to open log file"
+	enterColumn          = "Please, enter a column name to change:\n"
+	enterValue           = "Please, enter a value and press enter:\n"
+	dropDBF              = "Please, drop only DBF files, try again"
+	enteredColumnValue   = "You entered column: %s, value: %s\n"
+	dropTheFiles         = "Please drop files on the executable file"
+	working              = "Processing file:"
+	creatingFoldersError = "Error occurred while creating folders"
+	fileSavedMessage     = "File %s has been successfully saved.\n"
+	panicMessage         = "Panic occurred:"
+	successMessage       = "Program completed successfully."
 )
 
 var logger *log.Logger
@@ -21,7 +34,7 @@ var logger *log.Logger
 func init() {
 	file, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
 	if err != nil {
-		fmt.Println("Failed to open log file:", err)
+		fmt.Printf("%s: %v", failedToOpen, err)
 		os.Exit(1)
 	}
 	logger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
@@ -36,18 +49,17 @@ func main() {
 	checkForDroppedFiles(os.Args)
 	checkForOtherFormats(os.Args)
 
-	fmt.Printf("please enter a column name to change:\n")
+	fmt.Printf("%s", enterColumn)
 	_, err := fmt.Scan(&column)
 	if err != nil {
 		logger.Println(err)
 	}
-	fmt.Printf("please enter a value and press enter:\n")
+	fmt.Printf("%s", enterValue)
 	_, err = fmt.Scan(&columnValue)
 	if err != nil {
 		logger.Println(err)
 	}
-	fmt.Printf("you entered column:%s, value:%s\n", column, columnValue)
-
+	fmt.Printf(enteredColumnValue, column, columnValue)
 	wg.Add(len(os.Args) - 1)
 	go func() {
 		for _, filePath := range os.Args[1:] {
@@ -56,16 +68,17 @@ func main() {
 	}()
 
 	wg.Wait()
+	fmt.Println(successMessage)
 	<-time.After(15 * time.Second)
 }
 
 func processDBFFile(filePath string, col string, val string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Printf("processing file: %s\n", filePath)
+	fmt.Printf("%s %s\n", working, filePath)
 	_, fileName := filepath.Split(filePath)
 	dirPath := filepath.Dir(filePath)
-	changedFilesDir := filepath.Join(dirPath, "changed")
-	pathForTheChangedFiles := filepath.Join(dirPath, "changed", fileName)
+	changedFilesDir := filepath.Join(dirPath, outputFolder)
+	pathForTheChangedFiles := filepath.Join(dirPath, outputFolder, fileName)
 
 	t, err := dbf.LoadFile(filePath)
 	if err != nil {
@@ -75,7 +88,7 @@ func processDBFFile(filePath string, col string, val string, wg *sync.WaitGroup)
 	for i := 0; i < t.NumRecords(); i++ {
 		defer func() {
 			if r := recover(); r != nil {
-				logger.Println("panic occurred:", r)
+				logger.Printf(panicMessage, r)
 			}
 		}()
 		t.SetFieldValueByName(i, col, val)
@@ -83,9 +96,9 @@ func processDBFFile(filePath string, col string, val string, wg *sync.WaitGroup)
 
 	_, err = os.Stat(changedFilesDir)
 	if os.IsNotExist(err) {
-		err := os.MkdirAll(changedFilesDir, os.ModePerm)
+		err = os.MkdirAll(changedFilesDir, os.ModePerm)
 		if err != nil {
-			logger.Println("error creating folders:", err)
+			logger.Println(creatingFoldersError, err)
 			return
 		}
 	}
@@ -94,19 +107,19 @@ func processDBFFile(filePath string, col string, val string, wg *sync.WaitGroup)
 	if err != nil {
 		logger.Println(err)
 	}
-	fmt.Printf("file %s has been saved\n", pathForTheChangedFiles)
+	fmt.Printf(fileSavedMessage, pathForTheChangedFiles)
 }
 func checkForDroppedFiles(files []string) {
 	for len(files) < 2 {
-		fmt.Println("please drop files on the EXE file")
+		fmt.Printf("%s", dropTheFiles)
 		<-time.After(2 * time.Second)
 		os.Exit(0)
 	}
 }
 func checkForOtherFormats(files []string) {
 	for _, filePath := range files[1:] {
-		if !strings.HasSuffix(filePath, "DBF") {
-			fmt.Println("please drop only DBF files, try again")
+		if !strings.HasSuffix(filePath, fileExt) {
+			fmt.Printf("%s", dropDBF)
 			<-time.After(2 * time.Second)
 			os.Exit(0)
 		}
