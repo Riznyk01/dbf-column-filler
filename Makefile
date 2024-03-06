d867d8cc22df347b46b5ceb02901d1f1adeabfb1
Oleksandr Riznyk <OleksandrRiznyk01@gmail.com>
2024-03-06 11:42:00 +0200

    Add multiple columns processing.

---
+++ Makefile
@@ -0,0 +1,2 @@
+buildwin:
+	go build -o filler.exe cmd/main.go
--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"dbf-column-filler/internal/text"
 	"fmt"
 	"github.com/Riznyk01/dbf"
 	"log"
@@ -11,30 +13,12 @@ import (
 	"time"
 )
 
-const (
-	startMessage         = "Hello 👋 \nThis program is designed to modify column values in either a single file or a group of files that you drag and drop onto the executable file. \nSimply drag and drop the files onto the executable, enter the field name, and specify the new value. \nThe modified files will be saved in the 'changed' directory within the same folder where the original files are located.\n"
-	outputFolder         = "changed"
-	logFileName          = "error_log.txt"
-	fileExt              = "DBF"
-	failedToOpen         = "Failed to open log file"
-	enterColumn          = "Please, enter a column name to change:\n"
-	enterValue           = "Please, enter a value and press enter:\n"
-	dropDBF              = "Please, drop only DBF files, try again"
-	enteredColumnValue   = "You entered column: %s, value: %s\n"
-	dropTheFiles         = "Please drop files on the executable file"
-	working              = "Processing file:"
-	creatingFoldersError = "Error occurred while creating folders"
-	fileSavedMessage     = "File %s has been successfully saved.\n"
-	panicMessage         = "Panic occurred:"
-	successMessage       = "Program completed successfully."
-)
-
 var logger *log.Logger
 
 func init() {
-	file, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	file, err := os.OpenFile(text.LogFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
 	if err != nil {
-		fmt.Printf("%s: %v", failedToOpen, err)
+		fmt.Printf("%s: %v", text.FailedToOpen, err)
 		os.Exit(1)
 	}
 	logger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
@@ -42,43 +26,49 @@ func init() {
 
 func main() {
 	var wg sync.WaitGroup
-	var columnValue, column string
+	var params []string
 
-	fmt.Printf(startMessage)
+	fmt.Printf(text.StartMessage)
 
 	checkForDroppedFiles(os.Args)
 	checkForOtherFormats(os.Args)
 
-	fmt.Printf("%s", enterColumn)
-	_, err := fmt.Scan(&column)
-	if err != nil {
-		logger.Println(err)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var line string
+	for {
+		scanner.Scan()
+		line = scanner.Text()
+
+		params = strings.Split(line, " ")
+		if len(params)%2 != 0 {
+			fmt.Printf(text.EnterEven)
+		} else if len(params) == 0 {
+			fmt.Printf(text.DidntEnter)
+		} else {
+			break
+		}
+
 	}
-	fmt.Printf("%s", enterValue)
-	_, err = fmt.Scan(&columnValue)
-	if err != nil {
-		logger.Println(err)
-	}
-	fmt.Printf(enteredColumnValue, column, columnValue)
 	wg.Add(len(os.Args) - 1)
 	go func() {
 		for _, filePath := range os.Args[1:] {
-			go processDBFFile(filePath, column, columnValue, &wg)
+			go processDBFFile(filePath, params, &wg)
 		}
 	}()
 
 	wg.Wait()
-	fmt.Println(successMessage)
+	fmt.Println(text.SuccessMessage)
 	<-time.After(15 * time.Second)
 }
 
-func processDBFFile(filePath string, col string, val string, wg *sync.WaitGroup) {
+func processDBFFile(filePath string, par []string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Printf("%s %s\n", working, filePath)
+	fmt.Printf("%s %s\n", text.Working, filePath)
 	_, fileName := filepath.Split(filePath)
 	dirPath := filepath.Dir(filePath)
-	changedFilesDir := filepath.Join(dirPath, outputFolder)
-	pathForTheChangedFiles := filepath.Join(dirPath, outputFolder, fileName)
+	changedFilesDir := filepath.Join(dirPath, text.OutputFolder)
+	pathForTheChangedFiles := filepath.Join(dirPath, text.OutputFolder, fileName)
 
 	t, err := dbf.LoadFile(filePath)
 	if err != nil {
@@ -88,17 +78,19 @@ func processDBFFile(filePath string, col string, val string, wg *sync.WaitGroup)
 	for i := 0; i < t.NumRecords(); i++ {
 		defer func() {
 			if r := recover(); r != nil {
-				logger.Printf(panicMessage, r)
+				logger.Printf(text.PanicMessage, r)
 			}
 		}()
-		t.SetFieldValueByName(i, col, val)
+		for j := 0; j < len(par); j += 2 {
+			t.SetFieldValueByName(i, par[j], par[j+1])
+		}
 	}
 
 	_, err = os.Stat(changedFilesDir)
 	if os.IsNotExist(err) {
 		err = os.MkdirAll(changedFilesDir, os.ModePerm)
 		if err != nil {
-			logger.Println(creatingFoldersError, err)
+			logger.Println(text.CreatingFoldersError, err)
 			return
 		}
 	}
@@ -107,19 +99,19 @@ func processDBFFile(filePath string, col string, val string, wg *sync.WaitGroup)
 	if err != nil {
 		logger.Println(err)
 	}
-	fmt.Printf(fileSavedMessage, pathForTheChangedFiles)
+	fmt.Printf(text.FileSavedMessage, pathForTheChangedFiles)
 }
 func checkForDroppedFiles(files []string) {
 	for len(files) < 2 {
-		fmt.Printf("%s", dropTheFiles)
+		fmt.Printf("%s", text.DropTheFiles)
 		<-time.After(2 * time.Second)
 		os.Exit(0)
 	}
 }
 func checkForOtherFormats(files []string) {
 	for _, filePath := range files[1:] {
-		if !strings.HasSuffix(filePath, fileExt) {
-			fmt.Printf("%s", dropDBF)
+		if !strings.HasSuffix(filePath, text.FileExt) {
+			fmt.Printf("%s", text.DropDBF)
 			<-time.After(2 * time.Second)
 			os.Exit(0)
 		}
